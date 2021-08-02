3b7c10d6dd8861ef6f9d65f6b0fef5cb5dda6f19
Ahmed Rayf <ahmedrayf@hotmail.com>
2021-08-02 15:21:40 +0200

    Initial commit

---
+++ src/main/java/TextTranslate.java
@@ -0,0 +1,79 @@
+
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+
+public class TextTranslate {
+    public static void main(String[] args) throws IOException {
+
+        GoogleCredentials credentials =
+                GoogleCredentials.fromStream(new FileInputStream("F:\\Environment\\api-project-204667169246-bab65b95321a.json"));
+        credentials.refreshIfExpired();
+        AccessToken token = credentials.getAccessToken();
+
+
+        try (SpeechClient speechClient = SpeechClient.create()) {
+
+
+
+            // The path to the audio file to transcribe
+            String gcsUri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw";
+
+            // Builds the sync recognize request
+            RecognitionConfig config =
+                    RecognitionConfig.newBuilder()
+                            .setEncoding(AudioEncoding.LINEAR16)
+                            .setSampleRateHertz(16000)
+                            .setLanguageCode("en-US")
+                            .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setUri(gcsUri).build();
+
+            // Performs speech recognition on the audio file
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+
+            for (SpeechRecognitionResult result : results) {
+                // There can be several alternative transcripts for a given chunk of speech. Just use the
+                // first (most likely) one here.
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+            }
+        } try (SpeechClient speechClient = SpeechClient.create()) {
+
+            // The path to the audio file to transcribe
+            String gcsUri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw";
+
+            // Builds the sync recognize request
+            RecognitionConfig config =
+                    RecognitionConfig.newBuilder()
+                            .setEncoding(AudioEncoding.LINEAR16)
+                            .setSampleRateHertz(16000)
+                            .setLanguageCode("en-US")
+                            .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setUri(gcsUri).build();
+
+            // Performs speech recognition on the audio file
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+
+            for (SpeechRecognitionResult result : results) {
+                // There can be several alternative transcripts for a given chunk of speech. Just use the
+                // first (most likely) one here.
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+            }
+        }
+        
+    }
+}
